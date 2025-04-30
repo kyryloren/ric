8e3a72c6181d099e1a49c945195267a763f83559
kyryloren <dev@kyryloorlov.com>
2025-04-30 01:47:34 -0400

    Added meta data

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,40 @@
 import { Footer, Nav } from 'components'
 import { Hero, History, Team, Who } from './components'
 import { FAQ } from 'templates'
-import { fetchAPI } from 'lib'
+import { fetchAPI, getStrapiURL } from 'lib'
+
+export async function generateMetadata() {
+  const seoData = await fetchAPI('/about', {
+    populate: {
+      SEO: {
+        populate: '*',
+      },
+    },
+  })
+  const seoDoc = seoData?.data?.attributes.SEO
+
+  return {
+    title: seoDoc?.metaTitle || 'About',
+    description: seoDoc?.metaDescription,
+    keywords: seoDoc?.keywords?.split(', '),
+    alternates: {
+      canonical: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+    },
+    noIndex: seoDoc?.preventIndexing || false,
+    openGraph: {
+      title: seoDoc?.metaTitle,
+      description: seoDoc?.metaDescription,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+      images: {
+        url: getStrapiURL(
+          `${seoDoc?.metaImage?.data?.attributes?.url}?format=jpg&resize=1200x630`,
+        ),
+        width: 1200,
+        height: 630,
+      },
+    },
+  }
+}
 
 export default async function About() {
   const aboutData = await fetchAPI('/about', {
